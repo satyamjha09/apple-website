1a7a2b3766e760d8961806814273180dadd80265
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-04-15 13:41:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./componets/Hero"
+import Highlights from "./componets/Highlights"
+import Navbar from "./componets/Navbar"
+
+function App() {
+
+  return (
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      {/* <Model/> */}
+
+    </main>
+  )
+}
+
+export default App
